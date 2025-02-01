4c93f492959b11d49fb0882311f49535972c4296
barbssx <bsthefaani@gmail.com>
2025-01-31 22:44:11 -0300

    Commit and push

--- src/devdojo/introducao/Aula05EstruturasCondicionais.java
+++ src/devdojo/introducao/Aula05EstruturasCondicionais.java
@@ -4,6 +4,12 @@ public class Aula05EstruturasCondicionais {
     public static void main(String[] args) {
 
         //IF
+        int age = 15;
+
+        if (age < 18){
+        System.out.println("Minor");}
+        else{
+        System.out.println("Major");}
 
     }
 }
